f3e9d95d30fd7e1ef327eaf6981f2675ce917848
MorgV <erikhan03@mail.ru>
2024-12-26 09:25:25 +0300

    fix and report

--- EasyNamingPractice.js
+++ EasyNamingPractice.js
@@ -1,10 +1,15 @@
 function findingSumThreeBestBuyers(sumArray) {
   let sumResult = 0;
-  sumArray.forEach((i) => {
-    sumResult += i;
+  sumArray.forEach((num) => {
+    sumResult += num;
   });
   return sumResult;
 }
 
 let bestSumArray = [10000, 20000, 30000];
 console.log(findingSumThreeBestBuyers(bestSumArray));
+
+// Стало понятным:
+// 1 - чем занимается функция
+// 2 - какие данные принимает на вход
+// 3 - и с чем работает внутри функции
